2155b2c61efc4d1ee4d9351e21b96f4bf40e8b72
seangnewman <seangnewman@msn.com>
2021-08-07 14:21:48 -0700

    Review of basic configuration concepts

--- TennisBookings.Web/Pages/Index.cshtml.cs
+++ TennisBookings.Web/Pages/Index.cshtml.cs
@@ -8,10 +8,14 @@ namespace TennisBookings.Web.Pages
     public class IndexModel : PageModel
     {
         private readonly IGreetingService _greetingService;
+        private readonly IConfiguration _configuration;
+        private readonly IWeatherForecaster _weatherForecaster;
 
-        public IndexModel(IGreetingService greetingService)
+        public IndexModel(IGreetingService greetingService, IConfiguration configuration, IWeatherForecaster weatherForecaster)
         {
             _greetingService = greetingService;
+            _configuration = configuration;
+            _weatherForecaster = weatherForecaster;
         }
 
         public string Greeting { get; private set; }
@@ -22,7 +26,61 @@ namespace TennisBookings.Web.Pages
 
         public async Task OnGet()
         {
+            var homePageFeatures = _configuration.GetSection("Features:HomePage");
+
+            var features = new Features();
+            _configuration.Bind("Features:HomePage", features);
+
+            if (features.EnableGreeting)
+            {
+                Greeting = _greetingService.GetRandomGreeting();
+            }
+
+            ShowWeatherForecast = features.EnableWeatherForecast
+                && _weatherForecaster.ForecastEnabled;
+
+            if (ShowWeatherForecast)
+            {
+                var title = features.ForecastSectionTitle;
+                ForecastSectionTitle = string.IsNullOrEmpty(title) ? "How's the weather?" : title;
+
+                var currentWeather = await _weatherForecaster.GetCurrentWeatherAsync();
+
+                if (currentWeather != null)
+                {
+                    switch (currentWeather.Description)
+                    {
+                        case "Sun":
+                            WeatherDescription = "It's sunny right now.  A great day for tennis!";
+                            break;
+                        case "Cloud":
+                            WeatherDescription = "It's cloudy at the moment and the outdoor courts are in use.";
+                            break;
+                        case "Rain":
+                            WeatherDescription = "We're sorry but it's raining here.  No outdoor courts in use.";
+                            break;
+                        case "Snow":
+                            WeatherDescription = "It's snowing!  Outdoor courts will remain closed until further notice";
+                            break;
+
+
+                        default:
+                            WeatherDescription = "Unknows";
+                            break;
+                    }
+                }
+
+            }
+
             
+
+        }
+
+        private class Features
+        {
+            public bool EnableGreeting { get; set; }
+            public bool EnableWeatherForecast { get; set; }
+            public string ForecastSectionTitle { get; set; }
         }
     }
 }
