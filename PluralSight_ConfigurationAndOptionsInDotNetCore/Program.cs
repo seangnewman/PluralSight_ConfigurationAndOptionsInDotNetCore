0438cb05557f92970ba28ad8976266df403def73
seangnewman <seangnewman@msn.com>
2021-08-07 12:27:47 -0700

    Add project files.

---
+++ PluralSight_ConfigurationAndOptionsInDotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PluralSight_ConfigurationAndOptionsInDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
